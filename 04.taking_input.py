f5eb56def0a85cfa701c759b50056dbe32103967
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-05-17 14:12:37 +0530

    Add files via upload

---
+++ 04.taking_input.py
@@ -0,0 +1,11 @@
+name=input("Enter Name = ")
+print("Name = ",name)
+print()
+
+age=int(input("Enter int age = "))
+print("Age = ",age)
+print()
+
+price=float(input("Enter Float price = "))
+print("Price = ", price)
+print()
