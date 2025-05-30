00c2551ab38336519e73c8df75e2da753227ac53
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-05-30 13:58:55 +0530

    Add files via upload

---
+++ 14.rangefunc.py
@@ -0,0 +1,17 @@
+#Range function in for loop
+    #Range function returns sequence of nnumbers,
+    #starting from 0 (By default), Step means value to increase by 1 (by default)
+    #Range Function Syntax = Range (Start, Stop, Step)
+    #in that starting and step value is optional
+
+#Range (Stop)
+for i in range(5) :
+    print(i)
+
+#Range (Start, Stop)
+for i in range (1,5) :
+    print(i)
+
+#Range (Start, Stop, Step)
+for i in range (1,10,2) :
+    print(i)
